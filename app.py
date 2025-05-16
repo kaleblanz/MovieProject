35621cff61fd7c01b86f3dd440fcc4d40d5f71d2
Kaleb <kglanz2005@icloud.com>
2025-05-16 18:22:16 -0400

    first commit for movie proj, basic route added

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+
+#app object resprents our web app, instance of flask class
+app = Flask(__name__)
+
+#this route decorator connects this url to homeFunc()
+@app.route('/home') #string inside is the URL path: https://localhost:5000/home
+def homeFunc():
+    return "welcome to the home page of this movie proj"
+    #whatever is returned is what the visitor sees in their browser
+
+#ensures app is not restart manually if any changes are made in code
+if __name__ == '__main__':
+    #runs app in debug mode
+    app.run(debug=True)
