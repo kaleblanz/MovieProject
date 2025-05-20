24fa0a7f2ac0d78044f63a542617e96ffd616f04
Kaleb <kglanz2005@icloud.com>
2025-05-20 12:51:35 -0400

    added a seach route and search movie function

--- app.py
+++ app.py
@@ -1,15 +1,58 @@
 from flask import Flask 
+from flask import jsonify
+import requests
 
 #app object resprents our web app, instance of flask class
 app = Flask(__name__)
 
+url = 'https://api.themoviedb.org/3/movie/popular?api_key=0addcecf0cd16afdf3f9a70dbb13a388'
+
+
+
 #this route decorator connects this url to homeFunc()
-@app.route('/home') #string inside is the URL path: https://localhost:5000/home
+@app.route('/') #string inside is the URL path: https://localhost:5000/
 def homeFunc():
     return "welcome to the home page of this movie proj"
     #whatever is returned is what the visitor sees in their browser
+
+
+@app.route('/search/<string:movieTitle>') #movieTitle is a path parameter
+def search(movieTitle):
+    movie = search_movie(movieTitle)
+    if not movie: #movie is an empty list
+        return "The Movie Title of {movieTitle}, does not exist, sorry!"
+    else: #valid movie
+        #return jsonify(movie[0]["original_title"])
+        return jsonify(movie)
+
+
+
+def search_movie(movie):
+    url = "https://api.themoviedb.org/3/search/movie"
+    headers = {
+        "Accept" : "application/json" #format we want the response data (json)
+    }
+    params = {
+        "query" : movie,        #movie we're searching for
+        "api_key": "0addcecf0cd16afdf3f9a70dbb13a388",  #api key
+        "include_adult" : True, #include adult movies
+        "language" : "en-US",   #english language
+        "page" : 1              #only 1 page of results
+    }
+
+    response = requests.get(url,headers=headers,params=params)
+
+    if response.status_code == 200: 
+        #200 means the request was successful
+        data = response.json()
+        return data['results']
+    else:
+        print("error:", response.status_code) #went wrong
+        return []
+
 
 #ensures app is not restart manually if any changes are made in code
 if __name__ == '__main__':
     #runs app in debug mode
     app.run(debug=True)
+
