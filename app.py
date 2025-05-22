a1d1c05563c0b1d0811d0cec1200d66de2448b16
Kaleb <kglanz2005@icloud.com>
2025-05-22 12:03:59 -0400

    added ai scripting openai agent

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask 
-from flask import jsonify
+from openai import OpenAI
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
 import requests
 
 #app object resprents our web app, instance of flask class
 app = Flask(__name__)
 
-url = 'https://api.themoviedb.org/3/movie/popular?api_key=0addcecf0cd16afdf3f9a70dbb13a388'
-
-
 
 #this route decorator connects this url to homeFunc()
 @app.route('/') #string inside is the URL path: https://localhost:5000/
 def homeFunc():
-    return "welcome to the home page of this movie proj"
+    return aiscripting()
+    #return "welcome to the home page of this movie proj"
     #whatever is returned is what the visitor sees in their browser
 
 
@@ -20,7 +20,7 @@ def homeFunc():
 def search(movieTitle):
     movie = search_movie(movieTitle)
     if not movie: #movie is an empty list
-        return "The Movie Title of {movieTitle}, does not exist, sorry!"
+        return jsonify({"error": f"the movie {movieTitle} does not exist."})
     else: #valid movie
         #return jsonify(movie[0]["original_title"])
         return jsonify(movie)
@@ -34,7 +34,7 @@ def search_movie(movie):
     }
     params = {
         "query" : movie,        #movie we're searching for
-        "api_key": "0addcecf0cd16afdf3f9a70dbb13a388",  #api key
+        "api_key": os.getenv("TMDB_API_KEY"),  #api key
         "include_adult" : True, #include adult movies
         "language" : "en-US",   #english language
         "page" : 1              #only 1 page of results
@@ -50,6 +50,28 @@ def search_movie(movie):
         print("error:", response.status_code) #went wrong
         return []
 
+def aiscripting():
+    #load env variables from .env file
+    load_dotenv()
+
+    #get  the API key
+    api_key = os.getenv("OPENAI_API_KEY")
+    if not api_key:
+        raise ValueError("OPENAI KEY WAS NOT FOUND IN ENV")
+
+    #initialize the openai object
+    client = OpenAI(api_key=api_key)
+
+    #send prompt
+    response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": "Write a one-sentence bedtime story about a turtle."}
+    ]
+)
+
+    #print(response.choices[0].message.content)
+    return response.choices[0].message.content
 
 #ensures app is not restart manually if any changes are made in code
 if __name__ == '__main__':
